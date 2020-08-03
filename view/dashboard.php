a83199e7ebfe889e40fe24c96d81524bd6ab62c5
Andersson Weslley Felix de Souza <anderssonweslley123@gmail.com>
2020-08-03 12:31:36 -0300

    projeto base com login

---
+++ view/dashboard.php
@@ -0,0 +1,18 @@
+<?php require_once("layout/topTheme.php"); ?>
+<?php
+
+use \Phppot\Member;
+
+if (! empty($_SESSION["userId"])) {
+    require_once __DIR__ . './../class/Member.php';
+    $member = new Member();
+    $memberResult = $member->getMemberById($_SESSION["userId"]);
+        $displayName = $memberResult[0]["user_name"];
+}
+?>
+        <div class="dashboard">
+            <div class="member-dashboard">Welcome <b><?php echo $displayName; ?></b>, You have successfully logged in!<br>
+                Click to <a href="./logout.php" class="logout-button">Logout</a>
+            </div>
+        </div>
+<?php require_once("layout/bottomTheme.php"); ?>
---
+++ view/login-form.php
@@ -0,0 +1,68 @@
+<?php require_once("layout/topTheme.php"); ?>
+
+    <div>
+        <form action="login-action.php" method="post" id="frmLogin" onSubmit="return validate();">
+            <div class="demo-table">
+
+                <div class="form-head">Login</div>
+                <?php 
+                if(isset($_SESSION["errorMessage"])) {
+                ?>
+                <div class="error-message"><?php  echo $_SESSION["errorMessage"]; ?></div>
+                <?php 
+                unset($_SESSION["errorMessage"]);
+                } 
+                ?>
+                <div class="field-column">
+                    <div>
+                        <label for="username">Usiário</label><span id="user_info" class="error-info"></span>
+                    </div>
+                    <div>
+                        <input name="user_name" id="user_name" type="text"
+                            class="demo-input-box">
+                    </div>
+                </div>
+                <div class="field-column">
+                    <div>
+                        <label for="password">Senha</label><span id="password_info" class="error-info"></span>
+                    </div>
+                    <div>
+                        <input name="password" id="password" type="password"
+                            class="demo-input-box">
+                    </div>
+                </div>
+                <div class=field-column>
+                    <div>
+                        <input type="submit" name="login" value="Login"
+                        class="btnLogin"></span>
+                    </div>
+                </div>
+                <div class=field-column>
+                <button type="button" class="btn btn-primary">Primary</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script>
+    function validate() {
+        var $valid = true;
+        document.getElementById("user_info").innerHTML = "";
+        document.getElementById("password_info").innerHTML = "";
+        
+        var userName = document.getElementById("user_name").value;
+        var password = document.getElementById("password").value;
+        if(userName == "") 
+        {
+            document.getElementById("user_info").innerHTML = "required";
+        	$valid = false;
+        }
+        if(password == "") 
+        {
+        	document.getElementById("password_info").innerHTML = "required";
+            $valid = false;
+        }
+        return $valid;
+    }
+    </script>
+    
+<?php require_once("layout/bottomTheme.php"); ?>
