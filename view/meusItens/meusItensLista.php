cdcf84edca2ce136b268fa3a60c4d463ac6227e7
Andersson Weslley Felix de Souza <anderssonweslley123@gmail.com>
2020-08-13 08:17:51 -0300

    .

--- view/meusItens/meusItensLista.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("../layout/topTheme.php"); 
-
-if (! empty($_SESSION["user_name"])) {
-    require_once("../../class/Member.php"); 
-}else{
-    header("Location: auth/login-form.php");
-}
-?>
-<?php require_once("../layout/bottomTheme.php"); 
