d592690f0ab0c32c804e27cb60c417431f59d46d
William Pereira <william-pereira@hotmail.com>
2020-08-14 17:07:21 -0300

    Revert "Retirando o teste em css"
    This reverts commit e1a2788d8102cd9ab04f75eeb65de5d78bd32048.

--- view/layout/topTheme.php
+++ view/layout/topTheme.php
@@ -64,7 +64,7 @@
     <head>
         <title>User Login</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="../css/style.css" rel="stylesheet" type="text/css" />
+        <link href="./view/css/style.css" rel="stylesheet" type="text/css" />
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
